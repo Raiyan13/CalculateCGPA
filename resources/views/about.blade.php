b46d8f208c422868ad1e65b116f679928df95da9
Raiyan13 <tahminkabir13@gmail.com>
2021-01-30 00:11:44 +0600

    refactored codes

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -20,22 +20,22 @@
         </div>
     </nav>
     <div id="about">
-        <div id = "title">
+        <div id="title">
             <h1>CalculateCGPA</h1>
         </div>
         <div>
-            <h3 id = "details">Is For Calculating Your Result CGPA(Cumulative Grade Points Average) 
-            From Given Letter Grade. It Will Also Help You To Find The Grade Point. You Can Save Your 
-            Result Informations For Future Uses. CalculateCGPA Will Let You Update And Delete Your 
-            Informations Also.&#160; &#160; &#169; made by <i>S.m. Tahmin Kabir Raiyan.</i></h3>
+            <h3 id="details">Is For Calculating Your Result CGPA(Cumulative Grade Points Average)
+                From Given Letter Grade. It Will Also Help You To Find The Grade Point. You Can Save Your
+                Result Informations For Future Uses. CalculateCGPA Will Let You Update And Delete Your
+                Informations Also.&#160; &#160; &#169; made by <i>S.m. Tahmin Kabir Raiyan.</i></h3>
         </div>
-        <div id = "quote">
-            <h2><i>"There are no secrets to success. 
-            It is the result of preparation, hard work, and learning from failure."</i> 
-            &#160; &#160;- Colin Powell</h2>
+        <div id="quote">
+            <h2><i>"There are no secrets to success.
+                    It is the result of preparation, hard work, and learning from failure."</i>
+                &#160; &#160;- Colin Powell</h2>
         </div>
     </div>
-        
+
 
 </body>
 
